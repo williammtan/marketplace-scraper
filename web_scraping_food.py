5dc9914e2ea91edfb4a35c220f04beb9a35be601
rahman-kulina <rahman@kulina.id>
2021-04-07 10:12:01 +0700

    web scraping update

--- web_scraping_food.py
+++ web_scraping_food.py
@@ -98,7 +98,7 @@ def ingest_tokopedia_desc(category):
 
     # set csv
     today = date.today()
-    csv_file = open(f'data/detail/tokped_{category}.csv', 'w', encoding='utf-8') 
+    csv_file = open(f'data/tokped/tokped_{category}.csv', 'w', encoding='utf-8') 
     writer = csv.writer(csv_file)
 
     df = {}
@@ -219,9 +219,9 @@ def ingest_tokopedia():
         ingest_tokopedia_desc(ct3)
 
     
-def ingest_bl_link():
-    category = 'minuman'
-    url = f'https://www.bukalapak.com/c/food/{category}?page=5'
+def ingest_bukalapak():
+    category = 'bumbu'
+    url = f'https://www.bukalapak.com/c/food/{category}'
 
     # call open browser function
     chrome_options = Options()
@@ -242,17 +242,18 @@ def ingest_bl_link():
 
     # set csv
     # today = date.today()
-    csv_file = open(f'data/link_bl_{category}_2.csv', 'w', encoding='utf-8') 
+    csv_file = open(f'data/bukalapak/bl_{category}.csv', 'w', encoding='utf-8') 
     writer = csv.writer(csv_file)
 
     total_all = 0
     df = {}
     total_product = 0
-    for i in range(5, 30):
+    for i in range(1, 31):
         print(f"iterasi ke {i}")
-        driver.execute_script("window.scrollTo(0, 1500)")
-        driver.execute_script("window.scrollTo(0, 0)")
+        driver.execute_script("window.scrollTo(0, 1000)")
         time.sleep(2)
+        driver.execute_script("window.scrollTo(0, 0)")
+        time.sleep(1)
         
         try:
             linklists = None
@@ -304,6 +305,7 @@ def ingest_bl_link():
 
                     if k>=14:
                         driver.execute_script("window.scrollTo(0, 1500)")
+                        time.sleep(2)
                         pass
 
                 except Exception as e:
@@ -328,3 +330,310 @@ def ingest_bl_link():
 
     print("total halaman {} dan total item {}".format(total_all+1, total_product))
     driver.quit()
+
+def ingest_shopee():
+    category = 'Roti-Kue-cat.157.18449'
+    cat = category.split('-')
+    category_name = str(cat[0])+"_"+str(cat[1])
+    url = f'https://shopee.co.id/{category}'
+
+    # call open browser function
+    chrome_options = Options()
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.headless = True
+    # chrome_options.add_argument(--headless)
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--incognito')
+    chrome_options.add_argument("log-level=3")
+    # set user agent
+    ua = UserAgent()
+    userAgent = ua.random
+    chrome_options.add_argument(f'user-agent={userAgent}')
+    driver = webdriver.Chrome('../chromedriver', options=chrome_options)
+        # driver = webdriver.Chrome('../chromedriver_mac', options=chrome_options)
+    # open url
+    driver.get(url)
+
+    # set csv
+    # today = date.today()
+    csv_file = open(f'data/detail/shopee_{category_name}.csv', 'w', encoding='utf-8') 
+    writer = csv.writer(csv_file)
+
+    total_all = 0
+    df = {}
+    total_product = 0
+    for i in range(0, 51):
+        print(f"iterasi ke {i}")
+        time.sleep(2)
+        # driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        driver.execute_script("window.scrollTo(0, 2800)")
+        time.sleep(2)
+        driver.execute_script("window.scrollTo(0, 1750)")
+        time.sleep(2)
+        driver.execute_script("window.scrollTo(0, 0)")
+        time.sleep(2)
+        
+        try:
+            linklists = None
+            linklists = driver.find_elements_by_xpath('.//div[@class="col-xs-2-4 shopee-search-item-result__item"]//a[@href]')
+            linklists_total = len(linklists)
+            print(linklists_total)
+            time.sleep(1)
+            for k in range(0, linklists_total):
+                try:                        
+                    linklist = None
+                    linklist = WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.XPATH,'.//div[@class="col-xs-2-4 shopee-search-item-result__item"]//a[@href]')))
+                    url_w = linklist[k].get_attribute('href')
+                    print(str(k+1)+". "+url_w)
+
+                    # time.sleep(2)
+                    linklist[k].click()
+                    time.sleep(1)
+                    
+                    # scraping description
+                    try:
+                        title = driver.find_element_by_xpath('//div[@class="attM6y"]//span')
+                        title1 = title.text
+                    except (NoSuchElementException, StaleElementReferenceException) as e:
+                        title1 = ''
+                        pass
+
+                    try:
+                        desc = driver.find_element_by_xpath('.//div[@class="_3yZnxJ"]//span')
+                        desc1 = desc.text
+                    except (NoSuchElementException, StaleElementReferenceException) as e:
+                        desc1 = ''
+                        pass
+
+                    try:
+                        subcat = driver.find_elements_by_xpath('.//div[@class="flex items-center _1J-ojb"]//a[@href]')
+                        subcat1 = subcat[-1].text
+                    except (NoSuchElementException, StaleElementReferenceException) as e:
+                        subcat1 = ''
+                        pass
+                    
+                    df['title'] = title1
+                    df['description'] = desc1
+                    df['sub_category'] = subcat1
+                    df['category'] = category_name
+                    writer.writerow(df.values())
+
+                    driver.back()
+
+                    # if k>=29:
+                    #     driver.execute_script("window.scrollTo(0, 3000)")
+                    #     time.sleep(2)
+                    #     pass
+                    # elif k>=24 and k<39:
+                    #     driver.execute_script("window.scrollTo(0, 2400)")
+                    #     time.sleep(2)
+                    #     # pass
+                    # elif k>=39:
+                    #     driver.execute_script("window.scrollTo(0, 2700)")
+                    #     time.sleep(2)
+                    #     # pass
+
+                except Exception as e:
+                    print("failed scrap in product", e)
+                    time.sleep(1)
+                    pass
+
+            total_product = total_product+linklists_total
+
+        except Exception as e:
+            print("failed scrap in page", e)
+            time.sleep(1)
+            pass
+
+        j = i+1
+        url2 = f"https://shopee.co.id/{category}?page={j}"
+        driver.get(url2)
+
+    print("total halaman {} dan total item {}".format(total_all+1, total_product))
+    driver.quit()
+
+def ingest_shopee_link(category):
+    cat = category.split('-')
+    # category_name = str(cat[0])+"_"+str(cat[1])
+    category_name = str(cat[0])
+    url = f'https://shopee.co.id/{category}'
+
+    # call open browser function
+    chrome_options = Options()
+    chrome_options.add_argument("--window-size=1920,1080")
+    # chrome_options.headless = True
+    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--incognito')
+    chrome_options.add_argument("log-level=3")
+    # set user agent
+    # ua = UserAgent()
+    # userAgent = ua.random
+    # chrome_options.add_argument(f'user-agent={userAgent}')
+    driver = webdriver.Chrome('../chromedriver', options=chrome_options)
+    # open url
+    driver.get(url)
+
+    # set csv
+    # today = date.today()
+    csv_file = open(f'data/link_shopee_{category_name}.csv', 'w', encoding='utf-8') 
+    writer = csv.writer(csv_file)
+
+    total_all = 0
+    df = {}
+    total_product = 0
+    for i in range(0, 100):
+        print(f"iterasi ke {i}")
+        time.sleep(3)
+        # driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        driver.execute_script("window.scrollTo(0, 3000)")
+        time.sleep(2)
+        driver.execute_script("window.scrollTo(0, 1750)")
+        time.sleep(2)
+        driver.execute_script("window.scrollTo(0, 0)")
+        time.sleep(1)
+        
+        try:
+            linklists = None
+            linklists = driver.find_elements_by_xpath('.//div[@class="col-xs-2-4 shopee-search-item-result__item"]//a[@href]')
+            linklists_total = len(linklists)
+            print(linklists_total)
+            for k in range(0, linklists_total):
+                time.sleep(1)
+                try:                        
+                    # linklist = None
+                    # linklist = driver.find_elements_by_xpath('.//div[@class="col-xs-2-4 shopee-search-item-result__item"]//a[@href]')
+                    linklist = WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.XPATH, './/div[@class="col-xs-2-4 shopee-search-item-result__item"]//a[@href]')))
+                    url_w = linklist[k].get_attribute('href')
+                    print(str(k+1)+"."+url_w)
+                    df['link'] = url_w
+                    writer.writerow(df.values())
+
+                    total_product = total_product+k
+                except Exception as e:
+                    print("failed to get link", e)
+                    time.sleep(1)
+                    pass
+        except Exception as e:
+            print("failed to next page", e)
+            time.sleep(1)
+            pass
+
+        j = i+1
+        url2 = f"https://shopee.co.id/{category}?page={j}"
+        driver.get(url2)
+
+    # print("total halaman {} dan total item {}".format(total_all+1, total_product))
+    driver.quit()
+
+def ingest_shopee_desc(category):
+    # category = 'Makanan-Instan-cat.157.12561'
+    cat = category.split('-')
+    category_name = str(cat[0])+"_"+str(cat[1])
+    # url = f'https://shopee.co.id/{category}'
+
+    # call open browser function
+    chrome_options = Options()
+    chrome_options.add_argument("--window-size=1920,1080")
+    # chrome_options.headless = True
+    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--incognito')
+    # chrome_options.add_argument("log-level=3")
+    # prefs = {"profile.managed_default_content_settings.images":2,
+    #         "profile.default_content_setting_values.notifications":2,
+    #         "profile.managed_default_content_settings.stylesheets":2,
+    #         "profile.managed_default_content_settings.cookies":2,
+    #         "profile.managed_default_content_settings.javascript":1,
+    #         "profile.managed_default_content_settings.plugins":1,
+    #         "profile.managed_default_content_settings.popups":2,
+    #         "profile.managed_default_content_settings.geolocation":2,
+    #         "profile.managed_default_content_settings.media_stream":2,
+    # }
+    # chrome_options.add_experimental_option("prefs", prefs)
+    # set user agent
+    ua = UserAgent()
+    userAgent = ua.random
+    chrome_options.add_argument(f'user-agent={userAgent}')
+    driver = webdriver.Chrome('../chromedriver', options=chrome_options)
+    # open url
+    # driver.get(url)
+
+    # open data from link csv
+    data = pd.read_csv(f"data/link_shopee_{category_name}.csv", names=["url"])
+
+    # set csv
+    # today = date.today()
+    csv_file = open(f'data/shopee/shopee_{category_name}.csv', 'w', encoding='utf-8') 
+    writer = csv.writer(csv_file)
+
+    total_all = 0
+    df = {}
+    total_product = 0
+    for i, url_w in enumerate(data['url'].items()):
+        time.sleep(2)
+        try:                     
+            driver.get(str(url_w[1]))
+
+            # scraping description
+            try:
+                time.sleep(1)
+                title = driver.find_element_by_xpath('//div[@class="attM6y"]//span')
+                title1 = title.text
+            except (NoSuchElementException, StaleElementReferenceException) as e:
+                title1 = ''
+                pass
+
+            try:
+                time.sleep(1)
+                desc = driver.find_element_by_xpath('.//div[@class="_3yZnxJ"]//span')
+                desc1 = desc.text
+            except (NoSuchElementException, StaleElementReferenceException) as e:
+                desc1 = ''
+                pass
+
+            try:
+                time.sleep(1)
+                subcat = driver.find_elements_by_xpath('.//div[@class="flex items-center _1J-ojb"]//a[@href]')
+                subcat1 = subcat[-1].text
+            except (NoSuchElementException, StaleElementReferenceException) as e:
+                subcat1 = ''
+                pass
+            
+            print(str(i+1)+"."+title1)
+            df['title'] = title1
+            df['description'] = desc1
+            df['sub_category'] = subcat1
+            df['category'] = category_name
+            # print(df)
+            writer.writerow(df.values())
+
+            # total_product = total_product+k
+        except Exception as e:
+            print("failed to get data description", e)
+            time.sleep(1)
+            pass
+
+    # print("total halaman {} dan total item {}".format(total_all+1, total_product))
+    driver.quit()
+
+def ingest_shopee_full():
+    link_raw = [
+        # 'Bahan-Pokok-cat.157.14560',
+        # 'Roti-Kue-cat.157.18449',
+        # 'Cokelat-Permen-cat.157.14582',
+        'Susu-Olahan-cat.157.14574',
+        # 'Makanan-Minuman-Lainnya-cat.157.347'
+        # 'Minuman-cat.157.1445',
+    ]
+
+    for lr in link_raw:
+        print(lr)
+        # ingest_shopee_link(lr)
+        ingest_shopee_desc(lr)
