947396ab6905f47a7d4cf38f3ecb26b405001f10
WilliamTan2704 <william.h.tan27@gmail.com>
2021-11-23 11:10:12 +0700

    updated product-scraper-full.sh to use new system

--- product-discovery-scraper.sh
+++ product-discovery-scraper.sh
@@ -8,17 +8,13 @@ datetime=$(date +"%Y%m%d")
 run_dir="../data/runs-discovery/"
 current_products_file="$run_dir/products_current.csv"
 new_products_file="$run_dir/products.csv"
-new_products_full_file="$run_dir/products_full.jsonlines"
-new_products_preprocessed_file="$run_dir/products.preprocessed.jsonlines"
+current_table="food-id-app.external_data_temp.EXTERNAL_PRODUCTS"
+temp_table="food-id-app.external_data_temp.EXTERNAL_PRODUCTS_UPDATE_TEMP"
+update_table="food-id-app.external_data_temp.EXTERNAL_PRODUCTS_UPDATE"
 
 rm -rf "$run_dir"
 mkdir -p "$run_dir"
 
-bq query --format=csv --max_rows=300000000 --use_legacy_sql=false '
-SELECT
-  id, url as prod_url
-  FROM `food-id-app.external_data_temp.EXTERNAL_PRODUCTS`
-' > $current_products_file
 
 IFS=' ' read -ra CATS <<< $(cat ../categories.json | jq '. | map(.id) | join (" ")' | tr -d '"')
 for cat in "${CATS[@]}"
@@ -30,19 +26,29 @@ do
     python3 -m process.tokped_products -m $cat -o $category_dir
 done
 
-python3 -m process.merge_products $(find $run_dir -name "products.jsonlines") $current_products_file -c id prod_url -o $new_products_file
-scrapy crawl tokped_products -a product_list=$new_products_file -s LOG_LEVEL='INFO' -o $new_products_full_file
-python3 -m process.preprocess_products -p $new_products_full_file -o $new_products_preprocessed_file
+python3 -m process.merge_products $(find $run_dir -name "products.jsonlines") -c id prod_url -o $new_products_file -r 'prod_url:url'
 
 blob="gs://data_external_backup/upload/updates-discovery/$datetime/products.jsonlines"
-raw_blob="gs://data_external_backup/upload/updates-discovery/$datetime/products_raw.jsonlines"
-gsutil cp $new_products_preprocessed_file $blob
-gsutil cp $new_products_file $raw_blob
+gsutil cp $new_products_file $blob
 bq load \
   --replace \
   --source_format="NEWLINE_DELIMITED_JSON" \
-  "external_data_temp.EXTERNAL_PRODUCTS_UPDATE" \
+  $temp_table \
   $blob \
   ../product_schema.json
+
+bq query \
+    --replace \
+    --destination_table $update_table \
+    --use_legacy_sql=false \
+    "
+    SELECT id, url
+    FROM $current_table
+
+    UNION
+
+    SELECT id, url
+    FROM $temp_table
+    " # union $current_table and $temp_table to $update_table
 
 gcloud logging write scraper-log "Completed product-discovery-scraper update with $(wc -l < $new_products_preprocessed_file) products"
