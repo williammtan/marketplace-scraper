fa1cdc5cc011f77ed5db6da6dca30df59cfcd947
WilliamTan2704 <william.h.tan27@gmail.com>
2021-11-23 11:14:05 +0700

    added rename parameter to merge_products

--- scrapyfood/process/merge_products.py
+++ scrapyfood/process/merge_products.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from scrapyfood.utils import read_df
 
-def merge_products(files, output, columns=None):
+def merge_products(files, output, rename={}, columns=None):
     if columns is None:
         merged = pd.concat([
             read_df(f)
@@ -15,13 +15,19 @@ def merge_products(files, output, columns=None):
             for f in files
         ]).drop_duplicates()
 
-    merged.to_csv(output, index=False)
+    merged.rename(columns=rename).to_csv(output, index=False)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('files', nargs='+', help='Files to merge')
     parser.add_argument('-c', '--columns', nargs='+', help='Columns in which to merge (not required)', required=False, default=None)
+    parser.add_argument('-r', '--rename', nargs='+', help='columns to rename, col:renamed')
     parser.add_argument('-o', '--output', help='File to output')
     args = parser.parse_args()
 
-    merge_products(args.files, args.output, args.columns)
+    rename = {
+        r.split(':')[0]: r.split(':')[1]
+        for r in args.rename
+    }
+
+    merge_products(args.files, args.output, rename, args.columns)
