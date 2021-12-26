19a03ba4525a2fc232b165998bc09f98686625de
WilliamTan2704 <william.h.tan27@gmail.com>
2021-12-26 14:14:23 +0700

    reorder scraping

--- startup-script.sh
+++ startup-script.sh
@@ -3,13 +3,13 @@ cd /home/externalscraper/food_scraping/
 git stash
 git pull
 
+chmod +x category-growth-scraper.sh
+./category-growth-scraper.sh
+
 chmod +x product-scraper.sh
 ./product-scraper.sh
 
 chmod +x product-scraper-full.sh
 ./product-scraper-full.sh
-
-chmod +x category-growth-scraper.sh
-./category-growth-scraper.sh
 
 gcloud logging write scraper-log "Completed all startup-script tasks"
